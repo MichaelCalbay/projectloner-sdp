30189133af86433e2d2a6fd2ff0bf3baeeb7f81b
dzh7278autuni <111832982+dzh7278autuni@users.noreply.github.com>
2022-09-05 19:19:33 +1200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Sam Was HERE TWO',
+      title: 'Project Loner',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
