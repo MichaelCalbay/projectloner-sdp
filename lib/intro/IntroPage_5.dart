1ed74d60a2945075273f8c83ae5aed6d307fef7f
dzh7278 <samuel27418@gmail.com>
2022-09-14 12:21:16 +1200

    Onboarding View
    Guidiance and overview mock up

---
+++ lib/intro/IntroPage_5.dart
@@ -0,0 +1,16 @@
+// ignore: file_names
+import 'package:flutter/material.dart';
+
+class PageFive extends StatelessWidget {
+  const PageFive({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+    color: Colors.red,
+    child: const Center(
+      child: Text('Page Five'),     
+    ),
+    );
+  }
+}
