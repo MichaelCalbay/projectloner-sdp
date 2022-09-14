1501dee5e2952d5fe9409392f41f70dd059a5ff2
NelsonPalaganas07 <pyd9365@autuni.ac.nz>
2022-09-14 12:39:53 +1200

    login/signup done.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,13 +8,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:projectloner/RegisterPage.dart';
+// import 'package:projectloner/auth/RegisterPage.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const RegisterView());
-
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
