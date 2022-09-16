135ac8c87b8d4591e23912d05431365dd1da1c3b
NelsonPalaganas07 <pyd9365@autuni.ac.nz>
2022-09-16 13:16:06 +1200

    small verify tweakings.

--- lib/auth/VerifyEmail.dart
+++ lib/auth/VerifyEmail.dart
@@ -4,8 +4,6 @@ import 'dart:async';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:projectloner/views/onboarding_screen.dart';
-import 'package:projectloner/auth/LoginPage.dart';
 import 'package:projectloner/views/HomeView.dart';
 
 class VerifyEmailPage extends StatefulWidget {
