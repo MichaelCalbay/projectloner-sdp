e1353bea33f9b12614e9c86277f747f07670302e
dzh7278 <samuel27418@gmail.com>
2022-10-11 03:11:51 +1300

    Forums

--- lib/views/forum_view.dart
+++ lib/views/forum_view.dart
@@ -4,6 +4,8 @@ import 'package:projectloner/forum/list_post.dart';
 import 'package:projectloner/forum/posts_service.dart';
 import 'package:provider/provider.dart';
 
+import '../forum/posts_model.dart';
+
 class ForumScreen extends StatefulWidget {
   const ForumScreen({Key? key}) : super(key: key);
 
@@ -13,24 +15,20 @@ class ForumScreen extends StatefulWidget {
 }
 
 class _ForumScreenState extends State<ForumScreen> {
-  PostService _postService = PostService();
+  final PostService _postService = PostService();
   @override
   Widget build(BuildContext context) {
-    return StreamProvider.value(
-        value:
-        _postService.getPostsByUser(FirebaseAuth.instance.currentUser?.uid),
-      initialData: [],
-      child: Scaffold(
-        body: Container(
-          child: const ListPosts(),
-        ),
-      )
+    return  StreamProvider<List<PostModel>>.value(
+        value: _postService
+            .getPostsByUser(FirebaseAuth
+            .instance.currentUser?.uid),
+      initialData: const [],
+        child: Scaffold(
+          body: Container(
+            child: ListPosts(),
+          ),
+        )
     );
-
-
-
-
-
   }
 }
 
