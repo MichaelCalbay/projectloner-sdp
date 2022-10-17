ec2307687e5113a72927fb3618db4887f2cd7126
ShayneMuir <kmy5825@aut.ac.nz>
2022-10-17 16:17:24 +1300

    Stats 1.0
    Finally got stats working

--- lib/profile/profile.dart
+++ lib/profile/profile.dart
@@ -1,10 +1,23 @@
+<<<<<<< Updated upstream
 import 'dart:convert';
+=======
+import 'dart:async';
+import 'dart:convert';
+import 'dart:core';
+>>>>>>> Stashed changes
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+<<<<<<< Updated upstream
 import 'package:projectloner/profile/profile_widget.dart';
 import 'package:http/http.dart' as http;
+=======
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
+import '../widgets/profile_widget.dart';
+import 'package:projectloner/stats/user_stats.dart';
+>>>>>>> Stashed changes
 
 class ProfilePage extends StatefulWidget {
   @override
@@ -14,6 +27,7 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   String userfName = '';
   String userlName = '';
+<<<<<<< Updated upstream
 
   Future _getDataFromDataBase() async {
     await FirebaseFirestore.instance
@@ -28,12 +42,44 @@ class _ProfilePageState extends State<ProfilePage> {
         });
       }
     });
+=======
+  int? elo = 0;
+
+  Future getDataFromDataBase() async {
+    await FirebaseFirestore.instance
+        .collection('LonerUser')
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .get()
+        .then((snapshot) async {
+      if (snapshot.exists) {
+        setState(() {
+          userfName = snapshot.data()!["firstName"];
+          userlName = snapshot.data()!["lastName"];
+        });
+      }
+    });
+  }
+
+  Future getStats() async {
+    http.Response response = await http.get(Uri.parse(
+        "https://api.henrikdev.xyz/valorant/v1/mmr/ap/nismistrigis/oce"));
+    if (response.statusCode == 200) {
+      setState(() {});
+      UserStats userStats = UserStats.fromJson(jsonDecode(response.body));
+      elo = userStats.data!.elo;
+    }
+>>>>>>> Stashed changes
   }
 
   @override
   void initState() {
     super.initState();
+<<<<<<< Updated upstream
     _getDataFromDataBase();
+=======
+    getDataFromDataBase();
+    getStats();
+>>>>>>> Stashed changes
   }
 
   @override
@@ -65,7 +111,12 @@ class _ProfilePageState extends State<ProfilePage> {
           Text(
             FirebaseAuth.instance.currentUser!.email!,
             style: TextStyle(color: Colors.grey),
+<<<<<<< Updated upstream
           )
+=======
+          ),
+          Text(elo.toString()),
+>>>>>>> Stashed changes
         ],
       );
 }
--- lib/stats/user_stats.dart
+++ lib/stats/user_stats.dart
@@ -1,28 +1,106 @@
+<<<<<<< Updated upstream
 import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'dart:convert';
 
 class user_stats {
   final baseUrl = "https://api.henrikdev.xyz";
+=======
+class UserStats {
+  int? status;
+  Data? data;
 
-  Future<List<dynamic>> getUserStats(
-      String name, String tag, String location) async {
-    String url = "$baseUrl/valorant/v1/mmr/$location/$name/$tag";
+  UserStats({this.status, this.data});
 
-    final uri = Uri.parse(url);
+  UserStats.fromJson(Map<String, dynamic> json) {
+    status = json['status'];
+    data = json['data'] != null ? new Data.fromJson(json['data']) : null;
+  }
+>>>>>>> Stashed changes
 
-    try {
-      var response = await http.get(uri);
-
-      if (response.statusCode == 200) {
-        List<dynamic> fetchedStats = jsonDecode(response.body);
-
-        return fetchedStats;
-      } else {
-        return [];
-      }
-    } on Exception catch (e) {
-      return [];
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['status'] = this.status;
+    if (this.data != null) {
+      data['data'] = this.data!.toJson();
     }
+    return data;
+  }
+}
+
+class Data {
+  int? currenttier;
+  String? currenttierpatched;
+  Images? images;
+  int? rankingInTier;
+  int? mmrChangeToLastGame;
+  int? elo;
+  String? name;
+  String? tag;
+  bool? old;
+
+  Data(
+      {this.currenttier,
+      this.currenttierpatched,
+      this.images,
+      this.rankingInTier,
+      this.mmrChangeToLastGame,
+      this.elo,
+      this.name,
+      this.tag,
+      this.old});
+
+  Data.fromJson(Map<String, dynamic> json) {
+    currenttier = json['currenttier'];
+    currenttierpatched = json['currenttierpatched'];
+    images =
+        json['images'] != null ? new Images.fromJson(json['images']) : null;
+    rankingInTier = json['ranking_in_tier'];
+    mmrChangeToLastGame = json['mmr_change_to_last_game'];
+    elo = json['elo'];
+    name = json['name'];
+    tag = json['tag'];
+    old = json['old'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['currenttier'] = this.currenttier;
+    data['currenttierpatched'] = this.currenttierpatched;
+    if (this.images != null) {
+      data['images'] = this.images!.toJson();
+    }
+    data['ranking_in_tier'] = this.rankingInTier;
+    data['mmr_change_to_last_game'] = this.mmrChangeToLastGame;
+    data['elo'] = this.elo;
+    data['name'] = this.name;
+    data['tag'] = this.tag;
+    data['old'] = this.old;
+    return data;
+  }
+}
+
+class Images {
+  String? small;
+  String? large;
+  String? triangleDown;
+  String? triangleUp;
+
+  Images({this.small, this.large, this.triangleDown, this.triangleUp});
+
+  Images.fromJson(Map<String, dynamic> json) {
+    small = json['small'];
+    large = json['large'];
+    triangleDown = json['triangle_down'];
+    triangleUp = json['triangle_up'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['small'] = this.small;
+    data['large'] = this.large;
+    data['triangle_down'] = this.triangleDown;
+    data['triangle_up'] = this.triangleUp;
+    return data;
   }
 }
